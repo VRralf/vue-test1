b5c133b29e94c9c648c0de75b90df56b688e5f5f
VRralf <vrralf@gmail.com>
2022-10-27 18:14:24 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TodosView from './components/TodosView.vue';
+
+</script>
+
+<template>
+  
+  <TodosView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/InputNew.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const text = ref("");
+const emits = defineEmits(["onNewItem"])
+
+function handleSubmit(){
+    if(text !== ""){
+        //llamar emit
+        emits("onNewItem",text)
+        text.value = ""
+    }
+}
+
+</script>
+
+<template>
+    <form @submit.prevent="handleSubmit">
+        <input type="text" v-model="text">
+    </form>
+</template>
+
+<style scoped>
+
+form{
+    margin-bottom: 10px;
+}
+
+input{
+    width: 100%;
+    box-sizing: border-box;
+}
+
+</style>
